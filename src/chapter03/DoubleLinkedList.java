19d811cfe61b702a75305a3473de82d809f18099
Ghufran Syed <ghufran.syed@gmail.com>
2016-06-02 17:28:10 -0700

    add changes after review of book version

--- src/chapter03/DoubleLinkedList.java
+++ src/chapter03/DoubleLinkedList.java
@@ -28,71 +28,88 @@ public class DoubleLinkedList<T> {
         
     }
     
-    private Node<T> header = new Node<>(null, null, null);
-    private Node<T> tail = new Node<>(header, null, null);
-    header.next = tail;
+    // comparison between my version and book version: no constructor for DoubleLinkedList
+    private Node<T> header;
+    private Node<T> trailer;
+    private int size = 0 // no size variable declared
     
-    public Node<T> getFirst(){
+    public DoubleLinkedList<T> {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(header, null, null);
+        header.setNext(trailer);
+    }
+   
+    // no size() method declared
+    public int size(){ return size; }
+    
+    
+    public T getFirst(){
         if (isEmpty()) { return null; }
         else {
-            return header.getNext();
+            return header.getNext().getData(); // my method returned the node, they return the data from the node
         }
     }
     
-    public void addFirst(Node<T> n){
-        n.setNext() = header.getNext();
-        n.setPrevious(header);
-        header.setNext(n);
+    public void addFirst(T d){ // my method added a  node, their version adds the data
+        // since the node is an implementation detail, it makes more sense to add / return the data, not the node
+        
+        Node<T> newNode = new Node<>(header, header.getNext(), d);
+        header.setNext(newNode);
+        size++;
+        
+        
+        //n.setNext() = header.getNext();
+        //n.setPrevious(header);
+        // header.setNext(n);
         
     }
     
-    public Node<T> removeFirst(){
+    public T removeFirst(){
         if (isEmpty()) { return null; }
         else{
-            Node<T> result = header.getNext();
-            header.setNext(result.getNext());
+            Node<T> resultNode = header.getNext();
+            header.setNext(resultNode.getNext());
             header.getNext().setPrevious(header);
-            return result;
+            return resultNode.getData();
+            size--;
         }
         
     }
     
     
-    public Node<T> getLast(){
+    public T getLast(){
         if (isEmpty()) { return null; }
         else {
-            Node<T> result = tail.getPrevious();
+            return trailer.getPrevious().getData(); // changed to return data, not node
         }
     }
     
     public void addLast(Node<T> n){
-        Node<T> oldLast = tail.getPrevious();
+        Node<T> oldLast = trailer.getPrevious();
         n.setPrevious(oldLast);
-        n.setNext(tail);
+        n.setNext(trailer);
         oldLast.setNext(n);
-        tail.setPrevious(n);
+        trailer.setPrevious(n);
+        size++;
     }
+    
+    // they declare private addBetween and remove methods, then use those for the update methods e.g. addFirst, removeLast etc
     
     public Node<T> removeLast(){
         if (isEmpty()) { return null; }
         else {
             Node<T> result = getLast();
             Node<T> newLast = result.getPrevious();
-            newLast.setNext(tail);
-            tail.setPrevious(newLast);
+            newLast.setNext(trailer);
+            trailer.setPrevious(newLast);
+            size--;
             return result;
         }
         
     }
     
     public boolean isEmpty(){
-        return header.getNext() == tail;
+        return header.getNext() == trailer;
     }
-    
-    
-    
-    
-    
-
 
 }
